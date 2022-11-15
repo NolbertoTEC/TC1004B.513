c9e7f18b75087a9430b392362b92ddfa92a75934
NolbertoTEC <A01641501@itesm.mx>
2022-11-15 13:43:18 +0000

    new files

---
+++ firebaseclient.py
@@ -0,0 +1,14 @@
+from firebase_admin import firestore
+from firebase_admin import credentials
+
+#aplication Default credentials are automatically created
+
+cred = credentials.Certificate('credentials.json')
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+
+sensor_1_ref = db.collection(u'sensor_1')
+docs = sensor_1_ref.stream()
+
+for doc in docs:
+    print(f'{doc.id} => {doc.to_dict()}')
